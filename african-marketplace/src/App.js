8b11106491f7b48d9b2e15c287400fd93afb337b
katieolson84 <katieolson84@gmail.com>
2021-02-26 14:37:15 -0700

    Site setup

---
+++ african-marketplace/src/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+// Styling
+import './App.css';
+import "bootstrap/dist/css/bootstrap.css";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>African Marketplace</h1>
+      </header>
+    </div>
+  );
+}
+
+export default App;
