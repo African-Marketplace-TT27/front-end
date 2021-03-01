8ac29349dd9ef1aeec43f6d26504410447026fdc
Monica Zwissler <mzwissler@Monicas-MacBook-Air.local>
2021-03-01 15:45:37 -0800

    added coloring and styles to the items, positioning almost complete

--- african-marketplace/src/components/MarketingPage.js
+++ african-marketplace/src/components/MarketingPage.js
@@ -8,7 +8,10 @@ import logo from '../images/Sauti.png'
 import cart from '../images/ShoppingCartImg.png'
 import heroImg from '../images/Rectangle 33.png'
 import logoWhite from '../images/Sauti.white.png'
-import sellerImg from '../images/Rectangle 11.png'
+
+
+//component
+import SellerItem from './SellerItem'
 
 export default function MarketingPage() {
     const [sellerItems, setSellerItems] = useState([])
@@ -43,33 +46,11 @@ useEffect(()=>{
             <HeroImg className="heroImg">
                 <img src={heroImg} alt="woman grocery shopping"/>
             </HeroImg>
-            <div className="availableItems">
-                <div>
-                    <h2>Available Items</h2>
-                </div>
-                <div className="availableItemContainer">
-                    {sellerItems.map((item)=>{
-                        return <div className="sellerItem">
-                            {/* //Change the image below to be the api.image */}
-                            <img src={sellerImg} alt="Bell Peppers" />
-                            <div className="sellerItemDetails">
-                                <div>
-                                    <h6>{item.prodName ? item.prodName : "Bell Peppers"}</h6>
-                                    <h6>{item.price ? item.price : "$99.99"}</h6>
-                                </div>
-                                <div>
-                                    <p>{item.prodDesc ? item.prodDesc : "Blah Blah Blah Blah Blah" }</p>
-                                </div>
-                                <div>
-
-                                    <p>{item.country ? item.country : "Nairobi, Kenya"}</p>
-                                </div>
-                            </div>
-                        </div>
-                    })}
-                </div>
-            </div>
-            <footer>
+            <ItemsContainer>
+                <h2>Available Items</h2>
+                <SellerItem key={sellerItems.id} sellerItems={sellerItems}></SellerItem>
+            </ItemsContainer>
+            <Footer>
                 <div className="footerLogo">
                     <img src={logoWhite} alt="Sauti Logo"/>
                 </div>
@@ -79,19 +60,19 @@ useEffect(()=>{
                 <Link to='/help'>Help</Link>
                 </div>
 
-            </footer>
+            </Footer>
         </div>
     )
 }
 
 // NAVIGATION BAR STYLES
 const Header = styled.header`
-    border: 3px solid black;
+    /* border: 3px solid black; */
     width: 100%;
     display:flex;
     `
 const NavLogo = styled.div`
-    border: 3px solid green;
+    /* border: 3px solid green; */
     width: 15%;
     display:flex;
     align-items: center;
@@ -99,13 +80,13 @@ const NavLogo = styled.div`
     `
 
 const NavBar = styled.div`
-border: 3px solid orange;
+/* border: 3px solid orange; */
 display:flex;
 justify-content: flex-end;
 width:85%;
 
     .searchBar{
-        border: 2px dotted black;
+        /* border: 2px dotted black; */
         width: 30%;
         display:flex;
         align-items:center;
@@ -113,7 +94,7 @@ width:85%;
     }
     
     .loginLink{
-        border: 2px dotted turquoise;
+        /* border: 2px dotted turquoise; */
         width: 20%;
     }
 `
@@ -121,7 +102,22 @@ width:85%;
 //HERO IMAGE STYLES
 const HeroImg = styled.div`
     img{
-        border: 2px solid magenta;
+        /* border: 2px solid magenta; */
         width: 100%;
     }
 `
+
+// Item Container
+const ItemsContainer= styled.div`
+    background: #E5E5E5;
+    padding: 2%;
+
+    h2{
+        color: #C87D55;
+    }
+    `
+
+    //Footer
+    const Footer = styled.div`
+        background: #AD7C82;
+    `
