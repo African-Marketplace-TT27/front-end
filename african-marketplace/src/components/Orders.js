3f19e954a39e0be18ce76a3f82983b9dcffb29ef
katieolson84 <katieolson84@gmail.com>
2021-03-04 09:07:20 -0700

    fixed styling for reports, settings, and orders

--- african-marketplace/src/components/Orders.js
+++ african-marketplace/src/components/Orders.js
@@ -4,17 +4,34 @@ import React from 'react'
 import Navigation from './Navigation';
 import NavRight from './NavRight';
 
+// styles
+import styled from 'styled-components';
+
+const OrderContainer = styled.div`
+    display: flex;
+    flex-direction: column;
+    
+    .content{
+        text-align: center;
+        display: flex;
+        justify-content: center;
+    }
+    @media (min-width: 1024px){
+        flex-direction: row;
+    }
+`
+
 const Orders = () => {
     return (
-        <>
+        <OrderContainer>
         <div>
             <Navigation />
-            <NavRight />
         </div>
         <div className="content">
             <h1>Order Details Coming Soon!</h1>
         </div>
-        </>
+        <NavRight />
+        </OrderContainer>
     )
 }
 
--- african-marketplace/src/components/Settings.js
+++ african-marketplace/src/components/Settings.js
@@ -4,17 +4,33 @@ import React from 'react'
 import Navigation from './Navigation';
 import NavRight from './NavRight'
 
+// styles
+import styled from 'styled-components';
+
+const SettingsContainer = styled.div`
+    display: flex;
+    flex-direction: column;
+    
+    .content{
+        text-align: center;
+        display: flex;
+        justify-content: center;
+    }
+    @media (min-width: 1024px){
+        flex-direction: row
+    }
+`
 const Settings = () => {
     return (
-        <>
+        <SettingsContainer>
         <div>
             <Navigation />
-            <NavRight />
         </div>
         <div className="content">
             <h1>Setting Details Coming Soon!</h1>
         </div>
-        </>
+        <NavRight />
+        </SettingsContainer>
     )
 }
 
